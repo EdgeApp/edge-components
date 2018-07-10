f93219b847deafab62baa467ed1d3458d3438a75
Kevin Sullivan <kevin@airbitz.co>
2018-07-10 12:05:09 -0700

    add storybook, linting

---
+++ storybook/addons.js
@@ -0,0 +1,7 @@
+// @flow
+
+import '@storybook/addon-actions/register'
+import '@storybook/addon-links/register'
+import '@storybook/addon-options/register'
+import '@storybook/addon-knobs/register'
+import 'storybook-addon-jsx/register'
---
+++ storybook/storybook.js
@@ -0,0 +1,25 @@
+// @flow
+
+import { configure, getStorybookUI } from '@storybook/react-native'
+import React, { Component } from 'react'
+import { AppRegistry } from 'react-native'
+
+// import stories
+setTimeout(() => configure(() => require('./stories'), module), 1000) // setTimeout is a hack to get addons to work https://storybook.js.org/basics/faq/#why-is-there-no-addons-channel
+// configure(() => require('./stories'), module) // original
+
+// This assumes that storybook is running on the same host as your RN packager,
+// to set manually use, e.g. host: 'localhost' option
+const StorybookUIRoot = getStorybookUI({ port: 7007, onDeviceUI: true })
+
+// react-native hot module loader must take in a Class - https://github.com/facebook/react-native/issues/10991
+// https://github.com/storybooks/storybook/issues/2081
+// eslint-disable-next-line react/prefer-stateless-function
+class StorybookUIHMRRoot extends Component<{}> {
+  render () {
+    return <StorybookUIRoot />
+  }
+}
+
+AppRegistry.registerComponent('edgecomponents', () => StorybookUIHMRRoot)
+export default StorybookUIHMRRoot
