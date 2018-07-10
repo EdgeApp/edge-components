bc3e8cc6f98418404660f35527182ae30ef146a6
Kevin Sullivan <kevin@airbitz.co>
2018-07-10 13:02:07 -0700

    Remove broken imports, move deps -> devDeps

--- storybook/addons.js
+++ storybook/addons.js
@@ -2,6 +2,5 @@
 
 import '@storybook/addon-actions/register'
 import '@storybook/addon-links/register'
-import '@storybook/addon-options/register'
 import '@storybook/addon-knobs/register'
 import 'storybook-addon-jsx/register'
