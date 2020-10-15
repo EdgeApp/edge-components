6619e43e1c6f658dd2b2b40917fad54c1ea8b3d4
Art Manalili <artmanalili@gmail.com>
2020-10-16 06:09:46 +0800

    Remove Unused Components

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,10 +1,7 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
 
-import { IconButton } from './IconButton'
 import { PrimaryButton } from './PrimaryButton.js'
 import { SecondaryButton } from './SecondaryButton.js'
 import { TertiaryButton } from './TertiaryButton.js'
-import { TextAndIconButton } from './TextAndIconButton'
-import { TextButton } from './TextButton'
 
-export { PrimaryButton, SecondaryButton, TertiaryButton, IconButton, TextAndIconButton, TextButton }
+export { PrimaryButton, SecondaryButton, TertiaryButton }
--- src/Modals/index.js
+++ src/Modals/index.js
@@ -1,34 +1,16 @@
 // @flow
 
-import { PasswordInput } from './components'
 import { InputAndButtonStyle, MaterialInputStyle } from './components/styles.js'
 import { InputModal, createInputModal } from './InputModal/InputModal.js'
-import { Modal } from './Modal.js'
-import { ModalManager, showModal } from './ModalManager.js'
 import { styles as ModalStyle } from './ModalStyle.js'
 import { SecureTextModal, createSecureTextModal } from './SecureTextModal/SecureTextModal.js'
-import { SimpleConfirmModal, createSimpleConfirmModal } from './SimpleConfirmModal/SimpleConfirmModal.js'
-import { StaticModal, createStaticModal } from './StaticModal/StaticModal.js'
-import { createThreeButtonModal } from './ThreeButtonModal/ThreeButtonModal.js'
-import { YesNoModal, createYesNoModal } from './YesNoModal/YesNoModal.js'
 
 export {
-  YesNoModal,
-  createYesNoModal,
-  createThreeButtonModal,
   InputModal,
   createInputModal,
   SecureTextModal,
   createSecureTextModal,
-  PasswordInput,
-  StaticModal,
-  createStaticModal,
-  SimpleConfirmModal,
-  createSimpleConfirmModal,
-  ModalManager,
-  showModal,
   ModalStyle,
-  Modal,
   MaterialInputStyle,
   InputAndButtonStyle
 }
